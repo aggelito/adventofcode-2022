464339c517c436d80681b73b425748c9543d660c
stickeraugust <august@stickerapp.com>
2022-12-05 17:05:51 +0100

    prep day 5 input

--- 5/main.js
+++ 5/main.js
@@ -10,5 +10,19 @@ fs.readFile('example-input.txt', 'utf8', (err, data) => {
 
 
 async function start(input){
-    console.log(input);
+    const inputSections = input.replace(/(\r)/gm, "").split('\n\n');
+
+    const containerInputRows = inputSections[0].split('\n');
+    const actionsInput = inputSections[1].split('\n');
+
+    const nrOfColumns = (containerInputRows[0].length + 1) / 4;
+
+    console.log(nrOfColumns);
+
+    //prepare array of container stacks
+    for (const containerRow of containerInputRows){
+        for(let i = 0; i < nrOfColumns; i++){
+
+        }
+    }
 }
